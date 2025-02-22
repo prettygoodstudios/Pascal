833a97bae582f76ba5e69305223637890fc7a005
Miguel Rust <miguel.rust@usu.edu>
2025-02-22 10:36:32 -0700

    Updating build script.

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ rm -rf ./dist
 npx webpack --config webpack.config.js --mode production
 cp index.html ./dist
 cp manifest.webmanifest ./dist
+cp serviceworker.js ./dist
 mkdir ./dist/src
 cp -r ./src/images ./dist/src
